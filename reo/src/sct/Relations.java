cbd75c516f526635a1cba4a7170dcdb818f3b6f9
sung <jongmans@cwi.nl>
2016-07-19 17:13:48 +0200

    Initial Reo specification example

---
+++ reo/src/sct/Relations.java
@@ -0,0 +1,12 @@
+package sct;
+
+public class Relations {
+
+	public static boolean Reject(Object o) {
+		return "reject".equals(o);
+	}
+	
+	public static boolean Accept(Object o) {
+		return "accept".equals(o);
+	}
+}
